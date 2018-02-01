5ee98c1eb5f0156c859760124b74ced3d07dd62e
Mauricio Cardona J <mauriciocardona@seven4n.com>
2018-01-31 20:27:29 -0500

    Simulación mejorada

--- src/main/scala/com/example/roulette/RouletteMain.scala
+++ src/main/scala/com/example/roulette/RouletteMain.scala
@@ -1,73 +1,62 @@
 package com.example.roulette
 
 
-import scala.collection.mutable.ListBuffer
 import scala.util.Random
+import cats.implicits._
 
 object RouletteMain extends App {
 
+  case class Result(tabValue: Int, cash: Int)
+
   def randomNumber: Int = Random.nextInt(37)
 
-  def result(valueByRecord:Int,myRecords:List[List[Int]],number: Int) : Int =
-    myRecords.count(bet => bet.contains(number)) match {
-      case 0 => -(myRecords.size * valueByRecord)
-      case s => s * (valueByRecord * 9)
-    }
-
-  def startFoldingWhenWin(bet:List[List[Int]],cash:Int, tabValueAtFirst:Int, retries:Int): Int = {
-    var finalCash = cash
-    var tabValue = tabValueAtFirst
-    var a = 1
-    while(a <= retries){
-      val numberThatFell = randomNumber
-      val r = result(tabValue,bet,numberThatFell)
-      if(r > 0){
-        tabValue = tabValue * 2
-      }else tabValue = 100
-      finalCash = finalCash + r
-      if(finalCash < 0){
-        println(s"Perdio en el intento $a")
-        a = 101
-      }else{
-        println(s"Intento # $a ----> cayó el número => $numberThatFell  => dinero =>    $finalCash     valor de la ficha => $tabValue")
-        a = a + 1
-      }
-    }
-    finalCash
+  implicit class IntOptionConverter(val a: Int) extends AnyVal{
+    def toOption = if(a == 0) None else Some(a)
   }
 
-  def startFoldingBetWhenLost(bet:List[List[Int]],cash:Int, tabValueAtFirst:Int, retries:Int): Int = {
-    var finalCash = cash
-    var tabValue = tabValueAtFirst
-    var a = 1
-    while(a <= retries){
-      val numberThatFell = randomNumber
-      val r = result(tabValue,bet,numberThatFell)
-      if(r < 0){
-        tabValue = tabValue * 2
-      }else tabValue = tabValue
-      finalCash = finalCash + r
-      if(finalCash < 0){
-        println(s"Perdio en el intento $a => valor de la ficha => $tabValue")
-        a = 101
-      }else{
-        println(s"Intento # $a  cayo el numero $numberThatFell cash ----> $finalCash => valor de la ficha => $tabValue")
-        a = a + 1
-      }
-    }
-    finalCash
+  def result(tabValue:Int,myRecords:List[List[Int]],number: Int): Int =
+    myRecords.count(bet => bet.contains(number)).toOption.fold(-(myRecords.size * tabValue))(s => s * (tabValue * 9))
+
+  def foldW(bet: List[List[Int]],cash: Int,tabValue:Int, number: Int): Result = {
+    val r = result(tabValue,bet,number)
+    val finalCash = cash + r
+    if(r > 0) Result(tabValue * 2,finalCash) else Result(100,finalCash)
   }
 
-  def greater(f:Int,s:Int):Int = if(f > s) f else s
+  def foldL(bet: List[List[Int]],cash: Int,tabValue:Int, number: Int): Result = {
+    val r = result(tabValue,bet,number)
+    val finalCash = cash + r
+    if(r < 0) Result(tabValue * 2,finalCash) else Result(tabValue,finalCash)
+  }
 
-  def play = {
-    val cashToBet = 20000
-    val tabValue = 200
+  def play(cash: Int) = {
     val myRecords:List[List[Int]] = List(List(1,2,4,5),List(2,3,5,6),List(4,5,7,8),List(5,6,8,9),List(7,8,10,11),List(8,9,11,12))
-    val s = startFoldingWhenWin(myRecords,cashToBet,tabValue,100)
 
-    //val t = startFoldingBetWhenLost(myRecords, cashToBet, tabValue, 100)
-    println(s"quedaste con ==> $s")
+    var finalCash = cash
+    var tabValue = 100
+    var a = 1
+    while(a <= 100){
+      val number = randomNumber
+      if(finalCash > 0){
+        if(finalCash < 60000){
+          val f = foldW(myRecords,finalCash,tabValue, number)
+          tabValue = f.tabValue
+          finalCash = f.cash
+          println(s"W ---  Intento # $a ---- numero !! $number  !! ---- valor de ficha => $tabValue ----- dinero => $finalCash ")
+        }else{
+          val f = foldL(myRecords,finalCash,tabValue, number)
+          tabValue = f.tabValue
+          finalCash = f.cash
+          println(s"L ---  Intento # $a ---- numero !! $number  !! ---- valor de ficha => $tabValue ----- dinero => $finalCash ")
+        }
+      }else{
+        println(s"Perdiste en el intento # => $a")
+        a = 101
+      }
+      a = a + 1
+    }
+
+    println(s"quedaste con ==> $finalCash")
   }
-  play
+  play(20000)
 }
